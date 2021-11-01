d75e0da0464559e3b703a0eee7eb13b05cfa07c4
fabian varisco <fabianvarisco@gmail.com>
2021-11-01 19:04:05 -0300

    add exp_time and get_time generator

--- login.sh
+++ login.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 sso_xml="$(cat ./resources/sso.test.xml)"
-
 readonly sso_xml
 
-sso_xml_b64=$(echo "${sso_xml}" | base64 -w 0 | sed 's/+/-/g' | tr -d '=')
+gen_time=$(date +%s)
+readonly gen_time
+
+exp_time=$(( gen_time + 60 ))
+readonly exp_time
+
+sso_xml_b64=$(echo "${sso_xml}" | \
+              sed "s/{{GEN_TIME}}/${gen_time}/" | \
+              sed "s/{{EXP_TIME}}/${exp_time}/" | \
+              base64 -w 0 | sed 's/+/-/g' | tr -d '=')
 readonly sso_xml_b64
 
 data="foo=foo&bar=bar&token=${sso_xml_b64}&sign=XXXXX"
